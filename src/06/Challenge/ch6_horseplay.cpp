c1097faa561c85b6a50d31e00ca8df8f6bab2b2f
Jason R. Coombs <jaraco@jaraco.com>
2024-02-26 12:05:48 -0500

    solution 06

--- src/06/Challenge/ch6_horseplay.cpp
+++ src/06/Challenge/ch6_horseplay.cpp
@@ -10,17 +10,65 @@
 #include <vector>
 #include <string>
 
+
+class Move {
+private:
+    int rank_delta;
+    int file_delta;
+
+public:
+    Move(int rank_delta_in, int file_delta_in) : rank_delta(rank_delta_in), file_delta(file_delta_in) {}
+    int get_rank() const { return rank_delta; }
+    int get_file() const { return file_delta; }
+};
+
+class Square {
+private:
+    int rank;
+    int file;
+
+public:
+    Square(std::string pos) {
+        rank = static_cast<int>(pos[0]) - 'a';
+        file = static_cast<int>(pos[1]) - '1';
+    }
+
+    Square(int rank_in, int file_in) : rank(rank_in), file(file_in) {}
+
+    bool is_valid() {
+        return rank >= 0 && rank <= 7 && file >= 0 && file <= 7;
+    }
+
+    Square operator+(const Move& move) {
+        return Square(rank + move.get_rank(), file + move.get_file());
+    }
+
+    std::string to_string() {
+        std::string result = "";
+        result += static_cast<char>(rank) + 'a';
+        result += static_cast<char>(file + '1');
+        return result;
+    }
+};
+
 // knight_moves()
 // Summary: This function receives a string with the location of a knight in a chessboard and returns a vector of strings with the possible locations it might move to.
 // Arguments:
 //           knight: The knight's location.
 // Returns: An STL vector of strings with the possible locations to move.
 std::vector<std::string> knight_moves(std::string knight){
-    std::vector<std::string> moves;
+    std::vector<Move> moves {Move(-1, -2), Move(-2, -1), Move(-1, 2), Move(-2, 1), Move(1, -2), Move(2, -1), Move(1, 2), Move(2, 1)};
+    std::vector<std::string> dest;
 
     // Write your code here
+    Square start = Square(knight);
+    for(Move move: moves) {
+        if((start + move).is_valid()) {
+            dest.push_back((start + move).to_string());
+        }
+    }
 
-    return moves;
+    return dest;
 }
 
 // Main function
